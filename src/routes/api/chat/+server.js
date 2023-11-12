6f22fb19a0c42c8911ba7d97b4cdb684e9e9e866
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-11-12 18:52:29 +0100

    updated to latest ai model

--- src/routes/api/chat/+server.js
+++ src/routes/api/chat/+server.js
@@ -10,7 +10,7 @@ export const POST = async ({ request }) => {
 	const { messages } = await request.json();
 
 	const response = await openai.chat.completions.create({
-		model: 'gpt-3.5-turbo',
+		model: 'gpt-4-1106-preview',
 		stream: true,
 		messages
 	});
