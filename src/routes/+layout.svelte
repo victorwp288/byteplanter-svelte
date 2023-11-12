fb4ba1bef65fe313e04e4ae2a211cd534eb33ecb
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-11-12 18:42:27 +0100

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script>
 	import '../app.postcss';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
