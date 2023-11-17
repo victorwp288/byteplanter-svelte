63007e21215f7d5e74c8fdc9c3f80c3feb9206dc
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-11-17 17:17:14 +0100

    Add navigation drawer to layout and update sidebarLeft slot.

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,17 @@
+<script>
+	import {drawerStore} from '@skeletonlabs/skeleton'
+
+	function drawerClose() {
+		drawerStore.close()
+	}
+</script>
+
+<nav class="list-nav p-4">
+	<ul>
+		<li><a href="/">Notes</a></li>
+		<li><a href="/?test=1" on:click={drawerClose}>Other Page 1</a></li>
+		<li><a href="/?test=2" on:click={drawerClose}>Other Page 1</a></li>
+		<li><a href="/?test=3" on:click={drawerClose}>Other Page 1</a></li>
+
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,30 +3,53 @@
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 
-	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
-
+	import { AppShell, AppBar, Avatar, Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/components/Navigation.svelte';
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
 	inject({ mode: dev ? 'development' : 'production' });
+
+	function drawerOpen() {
+		drawerStore.Open();
+	}
 </script>
 
-<AppShell>
+<Drawer>
+	<Navigation />
+</Drawer>
+
+<AppShell slotSidebarLeft="w-0 md:w-52 bg-surface-500/10">
 	<svelte:fragment slot="header">
 		<AppBar>
-			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<svelte:fragment slot="lead">
+				<button class="md:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+					<span>
+						<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+							<rect width="100" height="20" />
+							<rect y="30" width="100" height="20" />
+							<rect y="60" width="100" height="20" />
+						</svg>
+					</span>
+				</button>
+				<strong class="text-xl uppercase">Noted</strong>
+			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<Avatar initials="JD" background="bg-primary-500" />
+				<Avatar initials="JD" width="10" background="bg-primary-500" />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
 	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
 	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
 	<!-- Router Slot -->
-	<slot />
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
 	<!-- ---- / ---- -->
 	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
 	<svelte:fragment slot="footer">Footer</svelte:fragment>
