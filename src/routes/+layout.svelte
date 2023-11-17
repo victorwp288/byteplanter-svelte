838230315fa81298813d328e4471814348d56335
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-11-17 13:51:02 +0100

    Add h-full and overflow-hidden to app.html and app.postcss, and update +layout.svelte with Skeleton UI components.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
@@ -11,4 +13,21 @@
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<Avatar initials="JD" background="bg-primary-500" />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
