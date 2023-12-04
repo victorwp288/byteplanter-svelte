5ee3e557bcdbbda87c7168b5d886c8884d521d10
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-12-04 21:12:01 +0100

    Refactor Firebase credentials handling

--- src/lib/server/admin.js
+++ src/lib/server/admin.js
@@ -1,18 +1,25 @@
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import {
-	FB_CLIENT_EMAIL,
-	FB_PRIVATE_KEY,
-	FB_PROJECT_ID,
-} from '$env/static/private';
+import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
 import pkg from 'firebase-admin';
 import firebase from 'firebase-admin';
 import serviceAccount from './service-account.json';
 
+const getGCPCredentials = () => {
+	return process.env.GCP_PRIVATE_KEY
+		? {
+				projectId: process.env.GCP_PROJECT_ID,
+				clientEmail: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+				privateKey: process.env.GCP_PRIVATE_KEY
+		  }
+		: {};
+};
+
 firebase.initializeApp({
-	credential: firebase.credential.cert(serviceAccount),
+	credential: firebase.credential.cert(getGCPCredentials()),
 	databaseURL: 'https://chats.firebaseio.com'
 });
+
 try {
 	pkg.initializeApp({
 		credential: pkg.credential.cert({
