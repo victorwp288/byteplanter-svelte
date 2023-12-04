2bbb93ffb108835476227503749b99a645ca046e
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-12-04 21:21:57 +0100

    Update Firebase Admin initialization in admin.js

--- src/lib/server/admin.js
+++ src/lib/server/admin.js
@@ -3,24 +3,19 @@ import { getFirestore } from 'firebase-admin/firestore';
 import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
 import pkg from 'firebase-admin';
 import firebase from 'firebase-admin';
-//import serviceAccount from './service-account.json';
-
-const getGCPCredentials = () => {
-	return process.env.GCP_PRIVATE_KEY
-		? {
-				projectId: process.env.GCP_PROJECT_ID,
-				clientEmail: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
-				privateKey: process.env.GCP_PRIVATE_KEY
-		  }
-		: {};
-};
+import serviceAccount from './service-account.json';
+import {GCP_PROJECT_ID, GCP_SERVICE_ACCOUNT_EMAIL, GCP_PRIVATE_KEY} from '$env/static/private';
 
 firebase.initializeApp({
-	credential: firebase.credential.cert(getGCPCredentials()),
+	credential: firebase.credential.cert({
+		projectId: GCP_PROJECT_ID,
+		clientEmail: GCP_SERVICE_ACCOUNT_EMAIL,
+		privateKey: GCP_PRIVATE_KEY.replace(/\\n/g, '\n')
+	}),
 	databaseURL: 'https://chats.firebaseio.com'
 });
 
-try {
+/*try {
 	pkg.initializeApp({
 		credential: pkg.credential.cert({
 			projectId: FB_PROJECT_ID,
@@ -32,7 +27,7 @@ try {
 	if (!/already exists/u.test(err.message)) {
 		console.error('Firebase Admin Error: ', err.stack);
 	}
-}
+}*/
 
 export const adminDB = getFirestore();
 export const adminAuth = getAuth();
