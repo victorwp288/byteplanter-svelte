66f0d4881a308b026eebb9f6549a6cef9a401314
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-12-04 21:32:35 +0100

    Remove unused code and update Firebase initialization

--- src/lib/server/admin.js
+++ src/lib/server/admin.js
@@ -1,9 +1,6 @@
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
-import pkg from 'firebase-admin';
 import firebase from 'firebase-admin';
-//import serviceAccount from './service-account.json';
 import {GCP_PROJECT_ID, GCP_SERVICE_ACCOUNT_EMAIL, GCP_PRIVATE_KEY} from '$env/static/private';
 
 firebase.initializeApp({
@@ -15,19 +12,6 @@ firebase.initializeApp({
 	databaseURL: 'https://chats.firebaseio.com'
 });
 
-/*try {
-	pkg.initializeApp({
-		credential: pkg.credential.cert({
-			projectId: FB_PROJECT_ID,
-			clientEmail: FB_CLIENT_EMAIL,
-			privateKey: FB_PRIVATE_KEY
-		})
-	});
-} catch (err) {
-	if (!/already exists/u.test(err.message)) {
-		console.error('Firebase Admin Error: ', err.stack);
-	}
-}*/
 
 export const adminDB = getFirestore();
 export const adminAuth = getAuth();
