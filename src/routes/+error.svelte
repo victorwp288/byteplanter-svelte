d9ad91a2527178da0daf76df509e0e024d43c0a1
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-12-06 22:34:16 +0100

    Remove header element from error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,6 @@
 <div
 	class="grid min-h-full grid-cols-1 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr]"
 >
-	<header
-		class="mx-auto w-full max-w-7xl px-6 pt-6 sm:pt-10 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:px-8"
-	>
-		<a href="#">
-			<span class="sr-only">Your Company</span>
-		</a>
-	</header>
 	<main
 		class="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8"
 	>
