3dfa40e858af65478d851b4abadf607435386d40
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-12-06 14:04:25 +0100

    Refactor contact and faq pages

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,4 +1,4 @@
-<div class="bg-white">
+<div>
 	<div class="mx-auto max-w-7xl px-6 py-24 sm:pt-32 lg:px-8 lg:py-40">
 		<div class="lg:grid lg:grid-cols-12 lg:gap-8">
 			<div class="lg:col-span-5">
@@ -7,7 +7,7 @@
 				</h2>
 				<p class="mt-4 text-base leading-7 text-gray-600">
 					Can’t find the answer you’re looking for? Reach out to our <a
-						href="#"
+						href="contact"
 						class="font-semibold text-indigo-600 hover:text-indigo-500">customer support</a
 					> team.
 				</p>
