c30f8aaf10071fdbbc56ceff8dc0ec6cd115376a
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-12-25 21:54:13 +0100

    Add experimental navigation link and update chat server code

--- src/routes/api/chat/+server.js
+++ src/routes/api/chat/+server.js
@@ -16,7 +16,11 @@ export const POST = async ({ request, cookies }) => {
 	const sessionCookie = cookies.get('__session');
 
 	const decodedClaims = await adminAuth.verifySessionCookie(sessionCookie);
+    const userId = decodedClaims.uid ?? 'Anonymous';
+	const userName = decodedClaims.name ?? 'Anonymous';
 
+	
+	
 	const response = await openai.chat.completions.create({
 		messages: [
 			{
@@ -39,8 +43,8 @@ export const POST = async ({ request, cookies }) => {
 				const docRef = await addDoc(collection(db, 'chats'), {
 					chat: completion,
 					time: new Date().toISOString(),
-					name: decodedClaims.name ?? 'Anonymous',
-					uid: decodedClaims.uid ?? 'Anonymous'
+					name: userName,
+					uid: userId,
 				});
 				console.log('Document written with ID: ', docRef.id);
 			} catch (e) {
