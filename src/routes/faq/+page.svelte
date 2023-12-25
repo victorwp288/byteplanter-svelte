e73e85ba8d228104d4b3e8c83eaca553efa6bbef
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2023-12-25 13:15:46 +0100

    Update FAQ page with improved formatting and content

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
-  <title>FAQ</title>
-  <meta name="description" content="Frequently asked questions">
+	<title>FAQ</title>
+	<meta name="description" content="Frequently asked questions" />
 </svelte:head>
 <div>
 	<div class="mx-auto max-w-7xl px-6 py-24 sm:pt-32 lg:px-8 lg:py-12">
@@ -23,25 +23,27 @@
 						<dd class="pb-6 mt-2 text-base leading-7 text-gray-300">
 							We use the latest model from openai, currently gpt-4-1106, also known as gpt-4-turbo
 						</dd>
-						<dt class="text-base font-semibold leading-7 text-white">
-							How do you handle my data?
-						</dt>
+						<dt class="text-base font-semibold leading-7 text-white">How do you handle my data?</dt>
 						<dd class="pb-6 mt-2 text-base leading-7 text-gray-300">
-							We store chats in a secure database, only for you to view them later, never for analytics
+							We store chats in a secure database, only for you to view them later, never for
+							analytics
 						</dd>
 						<dt class="text-base font-semibold leading-7 text-white">
 							Who are the person behind this project?
 						</dt>
 						<dd class="pb-6 mt-2 text-base leading-7 text-gray-300">
-							My name is Victor, and I am a web developer from Denmark. I am currently studying
-							web development at KEA in Copenhagen.
+							My name is Victor, and I am a web developer from Denmark. I am currently studying web
+							development at KEA in Copenhagen.
 						</dd>
-						<dt class="text-base font-semibold leading-7 text-white">
-							Is this site free to use?
-						</dt>
-						<dd class="mt-2 text-base leading-7 text-gray-300">
+						<dt class="text-base font-semibold leading-7 text-white">Is this site free to use?</dt>
+						<dd class="pb-6 mt-2 text-base leading-7 text-gray-300">
 							Yes! This site is free to use, and will always be free to use. If you want to support
 							me, you can buy me a coffee.
+						</dd>
+						<dt class="text-base font-semibold leading-7 text-white">How does it work?</dt>
+						<dd class="mt-2 text-base leading-7 text-gray-300">
+							Simply type into the textarea which data you want to generate, and the ai will generate
+							it for you.
 						</dd>
 					</div>
 
