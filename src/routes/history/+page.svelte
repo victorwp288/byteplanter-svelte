f76d5db8a3684da17947c53f3289aac79c653630
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2024-01-15 15:30:09 +0100

    Update query to include orderBy in history page

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,42 +1,47 @@
 <script>
-    import AuthCheck from '$lib/components/AuthCheck.svelte';
-    import { app } from '$lib/firebase.js';
-    import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
-    import { onMount } from 'svelte';
-    import { user } from '$lib/firebase';
+	import AuthCheck from '$lib/components/AuthCheck.svelte';
+	import { app } from '$lib/firebase.js';
+	import { getFirestore, collection, query, where, getDocs, orderBy } from 'firebase/firestore';
+	import { onMount } from 'svelte';
+	import { user } from '$lib/firebase';
 
-    const db = getFirestore(app);
+	const db = getFirestore(app);
 
-    let documentsData = [];
+	let documentsData = [];
 
-    onMount(async () => {
-        if ($user) {
-            console.log('uid', $user.uid);
-            console.log('displayName', $user.displayName);
-            const q = query(collection(db, 'chats'), where('uid', '==', $user.uid));
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-                console.log(doc.id, ' => ', doc.data());
-                documentsData = [...documentsData, doc.data()]; // This line is changed
-            });
-            console.log('documents data:', documentsData);
-        }
-    });
+	onMount(async () => {
+		if ($user) {
+			console.log('uid', $user.uid);
+			console.log('displayName', $user.displayName);
+			// Update the query to include orderBy
+			const q = query(
+				collection(db, 'chats'),
+				where('uid', '==', $user.uid),
+				orderBy('time') // Order by time field
+			);
+			const querySnapshot = await getDocs(q);
+			querySnapshot.forEach((doc) => {
+				console.log(doc.id, ' => ', doc.data());
+				documentsData = [...documentsData, doc.data()];
+			});
+			console.log('documents data:', documentsData);
+		}
+	});
 </script>
 
 <svelte:head>
-    <title>History</title>
-    <meta name="description" content="Log of chats" />
+	<title>History</title>
+	<meta name="description" content="Log of chats" />
 </svelte:head>
 <AuthCheck>
-    {#if documentsData.length}
-        {#each documentsData as documentData}
-            <div>
-                <h1>{documentData.chat}</h1>
-				<br/>
-            </div>
-        {/each}
-    {:else}
-        <div>Loading...</div>
-    {/if}
+	{#if documentsData.length}
+		{#each documentsData as documentData}
+			<div>
+				<h1>{documentData.chat}</h1>
+				<br />
+			</div>
+		{/each}
+	{:else}
+		<div>Loading...</div>
+	{/if}
 </AuthCheck>
